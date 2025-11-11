0461c1ce6eba7124eb501a5d5ba6b63b428b7db7
artemkuzmindevexpress <artem.kuzmin@devexpress.com>
2025-11-11 16:19:29 +0400

    Remove extra spaces in for loop declarations
    Standardized the formatting of for loop declarations by removing extra spaces after 'for' in moveGridDataCellContentToAnchors function for improved code consistency.

--- CS/blazor_kanban/blazor_kanban/Components/DxKanban/DxKanban.razor.js
+++ CS/blazor_kanban/blazor_kanban/Components/DxKanban/DxKanban.razor.js
@@ -1,8 +1,8 @@
 export function moveGridDataCellContentToAnchors() {
     const columnContainers = document.getElementsByClassName("kanban-column-grid");
-    for (const columnContainer of columnContainers) {
+    for(const columnContainer of columnContainers) {
         const dragAnchors = columnContainer.getElementsByClassName("kanban-drag-anchor");
-        for (const dragAnchor of dragAnchors) {
+        for(const dragAnchor of dragAnchors) {
             const gridContentCell = dragAnchor.nextElementSibling;
             dragAnchor.innerHTML = gridContentCell.innerHTML;
         }
