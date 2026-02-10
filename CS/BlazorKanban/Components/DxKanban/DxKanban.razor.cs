0dd5b45782dfd57a6b1980493ec8d1a871a1bdf1
svetlanamikheeva <svetlana.mikheeva@devexpress.com>
2026-02-10 16:29:34 +0400

    adjust code

--- CS/BlazorKanban/Components/DxKanban/DxKanban.razor.cs
+++ CS/BlazorKanban/Components/DxKanban/DxKanban.razor.cs
@@ -6,74 +6,75 @@ using System.Collections;
 using System.ComponentModel;
 using System.Reflection;
 
-namespace BlazorKanban.Components.DxKanban;
-public partial class DxKanban : ComponentBase, IAsyncDisposable {
-    #region Fields
-    private IEnumerable sampleSingleCellData = Enumerable.Range(0, 1);
-    private IJSObjectReference? jsModule;
-    #endregion
+namespace BlazorKanban.Components.DxKanban { 
+    public partial class DxKanban : ComponentBase, IAsyncDisposable {
+        #region Fields
+        private IEnumerable sampleSingleCellData = Enumerable.Range(0, 1);
+        private IJSObjectReference? jsModule;
+        #endregion
 
-    #region Services
-    [Inject]
-    private IJSRuntime JS { get; set; } = default!;
-    #endregion
+        #region Services
+        [Inject]
+        private IJSRuntime JS { get; set; } = default!;
+        #endregion
 
-    #region Parameters
-    [Parameter]
-    public IEnumerable? Data { get; set; }
+        #region Parameters
+        [Parameter]
+        public IEnumerable? Data { get; set; }
 
-    [Parameter]
-    public string? CssClass { get; set; }
+        [Parameter]
+        public string? CssClass { get; set; }
 
-    [Parameter]
-    public string? ColumnNameFieldName { get; set; }
+        [Parameter]
+        public string? ColumnNameFieldName { get; set; }
 
-    [Parameter]
-    public RenderFragment? Columns { get; set; }
+        [Parameter]
+        public RenderFragment? Columns { get; set; }
 
-    [Parameter]
-    public RenderFragment<object>? CardTemplate { get; set; }
+        [Parameter]
+        public RenderFragment<object>? CardTemplate { get; set; }
 
-    [Parameter]
-    public EventCallback<GridItemsDroppedEventArgs> CardDropped { get; set; }
-    #endregion
+        [Parameter]
+        public EventCallback<GridItemsDroppedEventArgs> CardDropped { get; set; }
+        #endregion
 
-    #region Event Handlers
-    private void ApplyCssClassesToHeaderAndDataCells(GridCustomizeElementEventArgs e) {
-        switch(e.ElementType) {
-            case GridElementType.HeaderCell:
-                e.CssClass = "kanban-header-cell";
-                break;
-            case GridElementType.DataCell:
-                e.CssClass = "kanban-data-cell";
-                break;
-        }
-    }
-    #endregion
-
-    #region Lifecycle Methods
-    protected override async Task OnAfterRenderAsync(bool firstRender) {
-        if(jsModule is null) {
-            jsModule = await JS.InvokeAsync<IJSObjectReference>("import", "/Components/DxKanban/DxKanban.razor.js");
-        }
-        await jsModule.InvokeVoidAsync("moveGridDataCellContentToAnchors");
-    }
-
-    public async ValueTask DisposeAsync() {
-        try {
-            if(jsModule != null) {
-                await jsModule.DisposeAsync();
+        #region Event Handlers
+        private void ApplyCssClassesToHeaderAndDataCells(GridCustomizeElementEventArgs e) {
+            switch(e.ElementType) {
+                case GridElementType.HeaderCell:
+                    e.CssClass = "kanban-header-cell";
+                    break;
+                case GridElementType.DataCell:
+                    e.CssClass = "kanban-data-cell";
+                    break;
             }
         }
-        catch(JSDisconnectedException) { }
-    }
-    #endregion
+        #endregion
 
-    #region Utility Methods
-    public void Refresh() => StateHasChanged();
+        #region Lifecycle Methods
+        protected override async Task OnAfterRenderAsync(bool firstRender) {
+            if(jsModule is null) {
+                jsModule = await JS.InvokeAsync<IJSObjectReference>("import", "/Components/DxKanban/DxKanban.razor.js");
+            }
+            await jsModule.InvokeVoidAsync("moveGridDataCellContentToAnchors");
+        }
 
-    private string GetGridCssClass() {
-        return $"kanban-layout-grid {CssClass}";
+        public async ValueTask DisposeAsync() {
+            try {
+                if(jsModule != null) {
+                    await jsModule.DisposeAsync();
+                }
+            }
+            catch(JSDisconnectedException) { }
+        }
+        #endregion
+
+        #region Utility Methods
+        public void Refresh() => StateHasChanged();
+
+        private string GetGridCssClass() {
+            return $"kanban-layout-grid {CssClass}";
+        }
+        #endregion
     }
-    #endregion
 }
